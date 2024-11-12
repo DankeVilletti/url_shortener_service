2c2a40e3f690fe4acb77b99fe0c2dcd95db2fee5
TheGreatLucky <danke_villetti@mail.ru>
2024-11-12 21:23:23 +0500

    add url tables

--- src/test/java/faang/school/url/shortener/UrlShortnerServiceApplicationTests.java
+++ src/test/java/faang/school/url/shortener/UrlShortnerServiceApplicationTests.java
@@ -3,11 +3,11 @@ package faang.school.url.shortener;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class UrlShortnerServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class UrlShortenerServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
