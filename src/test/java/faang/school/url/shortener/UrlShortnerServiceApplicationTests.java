fb8d17ae3bb74a254b6f2b21c4d3f3214db388cf
TheGreatLucky <danke_villetti@mail.ru>
2024-11-12 20:00:03 +0500

    Init Service

---
+++ src/test/java/faang/school/url/shortener/UrlShortnerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package faang.school.url.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
